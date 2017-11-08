c8df4f0c356946e37afe42f5ce5e2dbed0a7c126
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-08 14:07:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import ModuleDemo from 'qafrankly/FranklyModulesDemo';
+import ModuleDemo from 'qafrankly/WEB-1484-prod-child';
 
 class ModuleCrossDemo extends Component {
 
