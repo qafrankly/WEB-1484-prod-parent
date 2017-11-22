85f9ff6bd2af3ffb73c14883341e22400bd13a2e
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-22 17:34:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ import ModuleDemo from '.qafrankly/WEB-1484-prod-child';
 class ModuleCrossDemo extends Component {
 
   render(){
-	  
+	 
     return (
       <div>
         <h2>test Parent here</h2>
-	<h2>testing the first repo</h2>
-      </div>
-	    );
+	 </div>
+    );
 	  
   }
 }
